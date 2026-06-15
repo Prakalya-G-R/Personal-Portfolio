997a7dad0c41fa7093744ee5c18175c400422c68
Prakalya G R <grprakalya081107@gmail.com>
2026-06-15 18:10:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const toggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".nav-menu");
+const closeBtn = document.querySelector(".menu-close");
+
+toggle.addEventListener("click", () => {
+
+    menu.classList.add("active");
+    toggle.style.display = "none";
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    menu.classList.remove("active");
+    toggle.style.display = "flex";
+
+});
+
+
+const btn = document.getElementById("viewmorebtn");
+const cards = document.querySelectorAll(".projsmc");
+
+let visiblecards=3;
+if(cards.length<=3){
+    btn.style.display="none";
+}
+
+btn.addEventListener("click", () =>{
+    if(visiblecards>=cards.length){
+        visiblecards=3;
+        cards.forEach((cards,index) =>{
+            if(index>=3){
+                cards.style.display = "none";
+            }
+        });
+        btn.innerHTML = 'View More <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" /></svg>';
+    }
+
+    else{
+        visiblecards+=3;
+        
+        cards.forEach((cards, index) =>{
+            if(index<visiblecards){
+                cards.style.display = "flex";
+            }
+        });
+        if(visiblecards>=cards.length){
+            btn.innerHTML = 'Show Less <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" /></svg>';
+        }
+    }
+});
