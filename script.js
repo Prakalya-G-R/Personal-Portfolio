d288628f9040fc66fb1f76c8a828f7aaecb03c07
Prakalya G R <grprakalya081107@gmail.com>
2026-06-16 22:52:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -49,3 +49,27 @@ btn.addEventListener("click", () =>{
         }
     }
 });
+
+
+const certbtn=document.getElementById("viewmorecert");
+const certcards=document.querySelectorAll(".certsmcard");
+let visiblecard=6;
+if(certcards.length<=6){
+    certbtn.style.display="none";
+}
+certcards.forEach((cert, index) => {
+    if(index >= 6){
+        cert.style.display = "none";
+    }
+});
+let expanded=false;
+
+certbtn.addEventListener("click",()=>{
+    expanded=!expanded;
+    certcards.forEach((certs,index)=>{
+        if(index>=6){
+            certs.style.display=expanded?"flex":"none";
+        }
+    });
+    certbtn.innerHTML=expanded?'Show Less <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" /></svg>':'View More <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" /></svg>';
+});
